13afcfc7977d99c3e22200da8d7654c349fe1a22
milenaFO <mmilenaf@gmail.com>
2022-05-23 18:26:58 -0300

    add javascript

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fundamentos-web
+Testes e exercícios relacionados ao HTML, CSS e Javascript.
---
+++ assets/scripts/scriptAula.js
@@ -0,0 +1,27 @@
+/*var nome = "Turma 49 "
+
+    function hello (){
+        alert(nome + "está aprendendo JS") */
+    
+
+    /*let nome = window.document.getElementById('nome')
+    let email = document.getElementById('email')
+    let gata = document.getElementById('gata')
+    let assunto = document.getElementById ("assunto")
+
+
+
+    function verifica(){
+        if(nome.value == "" || email.value == "" || gata.value == "" || assunto.value == ""){ // || significa ou
+            alert("Todos os campos são obrigatórios")
+            return false
+        }
+        return true
+    }
+
+    function enviar (){
+        if(verifica()){
+            alert("Olá " + nome.value + "! E-mail enviado com sucesso")
+        }
+    }*/
+    
